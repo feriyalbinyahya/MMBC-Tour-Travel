710b0b10d863f22167a5a74ca566180419e1856b
Feriyalb <feriyalbinyahya@gmail.com>
2022-10-03 17:53:31 +0700

    push mmbc tour & travel flutter project

---
+++ android/app/src/main/kotlin/com/example/mmbc_tour_and_travel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mmbc_tour_and_travel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/main_home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: Text(
+          "MMBC Tour & Travel",
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
